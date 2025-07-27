19dbfe7743a3ee681a4f08d7c39cae38704882d5
Ankit Rai <161692587+ankitrai2304@users.noreply.github.com>
2025-07-27 21:45:36 +0530

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
 
 
 
 const App = () => {
   return (
     <div className='container mx-auto max-w-7xl'>
-    <Navbar/>
-    <Hero/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects/>
     </div>
   )
 }
