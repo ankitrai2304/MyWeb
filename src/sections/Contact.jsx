a82b207cc16670a0c72f3a9d33f4472ba9335d9c
Ankit Rai <161692587+ankitrai2304@users.noreply.github.com>
2025-08-23 00:26:55 +0530

    Fix styling issue in Contact component by adjusting className spacing

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
                 refresh
             />
             {showAlert && <Alert type={alertType} text={alertMessage} />}
-            <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
+            <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary ">
                 <div className="flex flex-col items-start w-full gap-5 mb-10">
                     <h2 className="text-heading">Let's Talk</h2>
                     <p className="font-normal text-neutral-400">
