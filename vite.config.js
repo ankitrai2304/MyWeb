4a1cde1e95889fdb0300e99aa4b9a62b42e045db
Ankit Rai <161692587+ankitrai2304@users.noreply.github.com>
2025-08-28 15:32:45 +0530

    optimize

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Optional: Increase warning threshold
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor'
+            }
+            if (id.includes('tailwindcss')) {
+              return 'tailwind'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
