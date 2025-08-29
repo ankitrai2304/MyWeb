07307b664965da2ef59a88f978ed30a7cfca75ec
Ankit Rai <161692587+ankitrai2304@users.noreply.github.com>
2025-08-29 16:01:43 +0530

    Add gh-pages dependency and update deployment scripts
    - Added "gh-pages" dependency to package.json for deployment purposes.
    - Updated scripts in package.json to include predeploy and deploy commands.
    - Set base path in vite.config.js for proper asset loading in GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"/MyWeb",
   build: {
     chunkSizeWarningLimit: 1000, // Optional: Increase warning threshold
     rollupOptions: {
