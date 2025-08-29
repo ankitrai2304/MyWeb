4e308bed0c3477346ea237a2b1fc26aca7cad904
Ankit Rai <161692587+ankitrai2304@users.noreply.github.com>
2025-08-29 16:07:50 +0530

    Refactor package.json and vite.config.js for improved formatting and clarity

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/MyWeb",
-  build: {
-    chunkSizeWarningLimit: 1000, // Optional: Increase warning threshold
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendor'
-            }
-            if (id.includes('tailwindcss')) {
-              return 'tailwind'
-            }
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
+  base:"/MyWeb"
 })
